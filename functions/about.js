18adbb9fca76b5a1fb28a8033bb135883030ce70
Sogo Ohta <sogo.ohta@eure.jp>
2023-04-16 02:20:33 +0900

    fix

--- functions/about.js
+++ functions/about.js
@@ -1,36 +1,33 @@
 const NAME = "myExampleWorkersABTest";
 
-// eslint-disable-next-line import/no-anonymous-default-export
-export default {
-  async fetch(req) {
-    const url = new URL(req.url);
+export async function onRequest(context) {
+  const url = new URL(req.url);
 
-    // Enable Passthrough to allow direct access to control and test routes.
-    if (url.pathname.startsWith("/control") || url.pathname.startsWith("/test"))
-      return fetch(req);
+  // Enable Passthrough to allow direct access to control and test routes.
+  if (url.pathname.startsWith("/control") || url.pathname.startsWith("/test"))
+    return fetch(req);
 
-    // Determine which group this requester is in.
-    const cookie = req.headers.get("cookie");
+  // Determine which group this requester is in.
+  const cookie = req.headers.get("cookie");
 
-    if (cookie && cookie.includes(`${NAME}=control`)) {
+  if (cookie && cookie.includes(`${NAME}=control`)) {
+    url.pathname = "/control" + url.pathname;
+  } else if (cookie && cookie.includes(`${NAME}=test`)) {
+    url.pathname = "/test" + url.pathname;
+  } else {
+    // If there is no cookie, this is a new client. Choose a group and set the cookie.
+    const group = Math.random() < 0.5 ? "test" : "control"; // 50/50 split
+    if (group === "control") {
       url.pathname = "/control" + url.pathname;
-    } else if (cookie && cookie.includes(`${NAME}=test`)) {
-      url.pathname = "/test" + url.pathname;
     } else {
-      // If there is no cookie, this is a new client. Choose a group and set the cookie.
-      const group = Math.random() < 0.5 ? "test" : "control"; // 50/50 split
-      if (group === "control") {
-        url.pathname = "/control" + url.pathname;
-      } else {
-        url.pathname = "/test" + url.pathname;
-      }
-      // Reconstruct response to avoid immutability
-      let res = await fetch(url);
-      res = new Response(res.body, res);
-      // Set cookie to enable persistent A/B sessions.
-      res.headers.append("Set-Cookie", `${NAME}=${group}; path=/`);
-      return res;
+      url.pathname = "/test" + url.pathname;
     }
-    return fetch(url);
-  },
-};
+    // Reconstruct response to avoid immutability
+    let res = await fetch(url);
+    res = new Response(res.body, res);
+    // Set cookie to enable persistent A/B sessions.
+    res.headers.append("Set-Cookie", `${NAME}=${group}; path=/`);
+    return res;
+  }
+  return fetch(url);
+}

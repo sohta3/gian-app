5ca9805b635af94600efb8734147dbfe609690c1
Sogo Ohta <sogo.ohta@eure.jp>
2023-04-15 12:31:52 +0900

    first commit

---
+++ pages/control/about.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+
+export default function About() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>About</h1>
+      <Image
+        className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+        src="/gian.jpg"
+        alt="gian"
+        width={180}
+        height={37}
+        priority
+      />
+      <div>おれはジャイアン</div>
+    </main>
+  );
+}

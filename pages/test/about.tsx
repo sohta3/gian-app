87bde2b64091f32861466f579026443748429d3d
Sogo Ohta <sogo.ohta@eure.jp>
2023-04-16 01:36:06 +0900

    fix

--- pages/test/about.tsx
+++ pages/test/about.tsx
@@ -1,19 +1,16 @@
-import { cloudflareLoader } from "@/libs/cloudflareLoader";
-import Image from "next/image";
-
 export default function About() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>About</h1>
-      <Image
-        className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-        loader={cloudflareLoader}
-        src="/beautiful_gian.jpg"
-        alt="beatiful gian"
-        width={180}
-        height={37}
-        priority
-      />
+      <picture>
+        <img
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          src="/beautiful_gian.jpg"
+          alt="beatiful gian"
+          width={180}
+          height={37}
+        />
+      </picture>
       <div>やあ、のび太くん</div>
     </main>
   );
